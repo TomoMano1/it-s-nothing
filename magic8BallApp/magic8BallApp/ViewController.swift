2eed73c79ab04b40818a6e06556ecd4b5891b303
TomoMano1 <58951687+TomoMano1@users.noreply.github.com>
2020-01-27 10:17:36 -0800

    Add files via upload

---
+++ magic8BallApp/magic8BallApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic8BallApp
+//
+//  Created by Guest User on 2020-01-27.
+//  Copyright © 2020 TomoMano1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ magic8BallApp/magic8BallAppTests/magic8BallAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  magic8BallAppTests.swift
+//  magic8BallAppTests
+//
+//  Created by Guest User on 2020-01-27.
+//  Copyright © 2020 TomoMano1. All rights reserved.
+//
+
+import XCTest
+@testable import magic8BallApp
+
+class magic8BallAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
